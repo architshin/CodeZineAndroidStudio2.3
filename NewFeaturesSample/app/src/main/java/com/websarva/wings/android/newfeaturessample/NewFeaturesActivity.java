4b46b8bed4c3a2700660175e504bd5234963c9ce
Shinzo SAITO <architshin@websarva.com>
2017-04-11 16:01:50 +0900

    「Android Studio2.3新機能サンプル」の追加。

---
+++ NewFeaturesSample/app/src/main/java/com/websarva/wings/android/newfeaturessample/NewFeaturesActivity.java
@@ -0,0 +1,22 @@
+package com.websarva.wings.android.newfeaturessample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * CodeZine
+ * Android Studioの新バージョン2.3をCheck It Out!
+ * Android Studio2.3新機能サンプル
+ *
+ * アクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
+public class NewFeaturesActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_features);
+	}
+}
