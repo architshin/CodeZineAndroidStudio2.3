67a5e376184a40ae9087377e32ff9a91a807489c
Shinzo SAITO <architshin@websarva.com>
2017-05-12 02:20:43 +0900

    「ConstraintLayoutサンプル」の追加。

---
+++ ConstraintLayoutSample/app/src/main/java/com/websarva/wings/android/constraintlayoutsample/ConstraintLayoutActivity.java
@@ -0,0 +1,22 @@
+package com.websarva.wings.android.constraintlayoutsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * CodeZine
+ * Android Studioの新バージョン2.3をCheck It Out!
+ * ConstraintLayoutサンプル
+ *
+ * アクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_constraint_layout);
+	}
+}
